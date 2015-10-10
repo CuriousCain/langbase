324585abb3059660dc31dda02271b2d57236e1c5
Joel <joel.lord@hotmail.co.uk>
2015-10-11 00:38:31 +0100

    Tests for getsentences and implementation

--- data/parser.go
+++ data/parser.go
@@ -3,7 +3,20 @@ package data
 import "strings"
 
 func GetSentences(inputText string) []string {
-	sentences := strings.Split(inputText, ".") //TODO: Don't only split on period
+	untrimmedSentences := strings.FieldsFunc(inputText, separateString) //TODO Recognize sentence type
+	return trimSpaces(untrimmedSentences)
+}
+
+func separateString(c rune) bool {
+	return c == '.' || c == '!' || c == '?'
+}
+
+func trimSpaces(inputSentences []string) []string {
+	var sentences []string
+
+	for _, sentence := range inputSentences {
+		sentences = append(sentences, strings.TrimSpace(sentence))
+	}
 
 	return sentences
 }
--- data/parser_test.go
+++ data/parser_test.go
@@ -5,10 +5,24 @@ import (
 )
 
 func TestGetSentences(t *testing.T) {
-	testInput := "Hello everybody. My name is Joel. What's your name, if I may ask?"
+	controlSentence := "My name is Joel"
+	controlLength := 4
+
+	testInput := "Hello everybody. My name is Joel. What's your name, if I may ask? Oh that's great!"
 	sentences := GetSentences(testInput)
 
-	if len(sentences) != 3 {
-		t.Fatal("Sentences length was incorrect. Got %s", len(sentences))
+	if len(sentences) != controlLength {
+		for _, s := range sentences {
+			t.Log(s)
+		}
+		t.Fatal("Sentences length was incorrect. Got", len(sentences))
 	}
+
+	if sentences[1] != controlSentence {
+		t.Fatal("Sentence does not match. Expected:", controlSentence, "but got:", sentences[1])
+	}
+}
+
+func TestGetPairs(t *testing.T) {
+
 }
